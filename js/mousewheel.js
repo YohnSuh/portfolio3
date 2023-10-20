e35b20cf6508e1075cead75c850988d747c270d3
YohnSuh <yohnsuh@gmail.com>
2023-10-20 16:47:53 +0900

    포폴3 제작 중

--- js/mousewheel.js
+++ js/mousewheel.js
@@ -1,13 +1,37 @@
-$('section').mousewheel(function(e, d) {
-    if(d < 0) {
-        let nextSection = $(this).next().offset().top;
-        $('html, body').stop().animate({
-            'scrollTop' : nextSection + 'px'
+// 페이지 로드 시 너비 변경
+if ($(window).width() >= 768) {
+    $('section').mousewheel(function(e, d) {
+        if (d < 0) {
+            let nextSection = $(this).next().offset().top;
+            $('html, body').stop().animate({
+                'scrollTop': nextSection + 'px'
+            });
+        } else if (d > 0) {
+            let prevSection = $(this).prev().offset().top;
+            $('html, body').stop().animate({
+                'scrollTop': prevSection + 'px'
+            });
+        }
+    });
+}
+
+// 창 너비 변경 시
+$(window).resize(function() {
+    if ($(window).width() >= 768) {
+        $('section').off('mousewheel').mousewheel(function(e, d) {
+            if (d < 0) {
+                let nextSection = $(this).next().offset().top;
+                $('html, body').stop().animate({
+                    'scrollTop': nextSection + 'px'
+                });
+            } else if (d > 0) {
+                let prevSection = $(this).prev().offset().top;
+                $('html, body').stop().animate({
+                    'scrollTop': prevSection + 'px'
+                });
+            }
         });
-    } else if(d > 0) {
-        let prevSection = $(this).prev().offset().top;
-        $('html, body').stop().animate({
-            'scrollTop' : prevSection + 'px'
-        });
+    } else {
+        $('section').off('mousewheel');
     }
 });
