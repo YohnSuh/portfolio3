5cfb31494cb4e2839251b956e879da06c73eb652
YohnSuh <yohnsuh@gmail.com>
2023-10-25 18:06:31 +0900

    header수정하기

--- js/header.js
+++ js/header.js
@@ -1,5 +1,7 @@
 // gng메뉴 아코디언으로 나타나게
 $('.gnb-menu h3').click(function() {
+    $(this).toggleClass('active');
+    $(this).parent().siblings().find('h3').removeClass('active');
     $(this).next().stop().slideToggle();
     $(this).parent().siblings().find('.gnb-in').stop().slideUp();
 });
--- js/mousewheel.js
+++ js/mousewheel.js
@@ -1,3 +1,34 @@
+$('.pager li').click(function() {
+    let idx = $(this).index();
+    let top = $('section').eq(idx).offset().top;
+    $('html, body').stop().animate({
+        'scrollTop' : top + 'px'
+    });
+});
+
+let main = $('.mainContainer').offset().top;
+if(main == 0) {$('.pager li').eq(0).addClass('on');}
+$(window).scroll(function() {
+    let windowTop = $(window).scrollTop();
+    let menu = $('.menuContainer').offset().top;
+    let category = $('.categoryContainer').offset().top;
+    let info = $('.infoContainer').offset().top;
+
+    if(windowTop >= 0 && windowTop < menu) {
+        $('.pager li').eq(0).addClass('on');
+        $('.pager li').eq(0).siblings().removeClass('on');
+    } else if(windowTop < category) {
+        $('.pager li').eq(1).addClass('on');
+        $('.pager li').eq(1).siblings().removeClass('on');
+    } else if(windowTop < info) {
+        $('.pager li').eq(2).addClass('on');
+        $('.pager li').eq(2).siblings().removeClass('on');
+    } else {
+        $('.pager li').eq(3).addClass('on');
+        $('.pager li').eq(3).siblings().removeClass('on');
+    }
+});
+
 // 페이지 로드 시 너비 변경
 if ($(window).width() >= 768) {
     $('section').mousewheel(function(e, d) {
